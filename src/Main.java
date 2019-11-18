8805649595c7fac2aed08fe2a1ebb128db3ec40d
yyhk123 <minoook@hotmail.com>
2019-11-17 19:41:14 -0800

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+
+public class Main {
+	
+
+	public static void main(String[] args) {
+		System.out.println("for github");
+		Board board = new Board();
+		Game game = new Game(board);
+		game.start();
+		
+	}
+	
+}
